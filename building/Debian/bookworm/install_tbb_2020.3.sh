b9fb2e9102cf2b1570dd474ad64fdd1d0ce3eb42
Guillaume GASNIER <2644355+fantomid@users.noreply.github.com>
2025-04-14 07:42:59 +0200

    Install TBB 2020.3

---
+++ building/Debian/bookworm/install_tbb_2020.3.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ $# -eq 3 ] ; then
+    echo 'Source and install directories are mandatory !'
+    exit 1
+fi
+
+if [ ! -d "$2/lib/cmake/tbb" ]; then
+  echo "$2/lib/cmake/tbb does not exist, create it."
+  mkdir $2/lib/cmake/tbb
+fi
+
+echo "Copy TBB include's files"
+cp -r $1/TBB-prefix/src/TBB/include/tbb $2/include/
+echo "Copy TBB cmake's files"
+cp -r $1/TBB-prefix/src/TBB/cmake/* $2/lib/cmake/tbb/
+
+CC_VERSION=`cc --version | grep "Debian" | cut -d" " -f4`
+BUILD_DIR=`ls $1/TBB-prefix/src/TBB/build | grep $CC_VERSION`
+
+echo "Copy TBB $BUILD_DIR lib's files"
+cp $1/TBB-prefix/src/TBB/build/$BUILD_DIR/libtbb.so.2 $2/lib/libtbb.so.2
+cp $1/TBB-prefix/src/TBB/build/$BUILD_DIR/libtbbmalloc.so.2 $2/lib/libtbbmalloc.so.2
+cp $1/TBB-prefix/src/TBB/build/$BUILD_DIR/libtbbmalloc_proxy.so.2 $2/lib/libtbbmalloc_proxy.so.2
